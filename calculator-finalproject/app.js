78ce79491242335c78ba6ae371cf39d9574deccd
Derek <derek141772@gmail.com>
2024-02-16 00:22:12 -0800

    cool iphone ui

--- calculator-finalproject/app.js
+++ calculator-finalproject/app.js
@@ -4,7 +4,8 @@ const multiply = (a,b) => {return a * b};
 const divide = (a,b) => {return a / b};
 
 const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display')
+const display = document.querySelector('.display');
+const timeDisplay = document.querySelector('.time');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -12,3 +13,16 @@ buttons.forEach(button => {
         display.textContent = button.id;
     });
 });
+
+const updateTime = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const timeString = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
+
+    timeDisplay.textContent = timeString;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
